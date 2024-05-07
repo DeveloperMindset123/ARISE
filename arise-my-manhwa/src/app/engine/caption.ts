23d87410754cd371ba352ce8c1f4f7ad02528779
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-07 06:08:42 -0400

    some cleanup for demo

--- arise-my-manhwa/src/app/engine/caption.ts
+++ arise-my-manhwa/src/app/engine/caption.ts
@@ -1,36 +1,33 @@
-"use server"
+"use server";
 
 import { ImageAnalysisRequest, ImageAnalysisResponse } from "@/types";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({path: '.env'})
-
-const apiUrl = `${process.env.RENDERING_VIDEOCHAIN_API_URL || ""}`
-console.log("The Api URL Loaded in caption.ts file is:", apiUrl);
+dotenv.config({ path: ".env" });
+const apiUrl = `${process.env.RENDERING_VIDEOCHAIN_API_URL || ""}`;
 
 export async function see({
   prompt,
-  imageBase64
+  imageBase64,
 }: {
-  prompt: string
-  imageBase64: string
+  prompt: string;
+  imageBase64: string;
 }): Promise<string> {
   if (!prompt) {
-    console.error(`cannot call the API without an image, aborting..`)
-    throw new Error(`cannot call the API without an image, aborting..`)
+    console.error(`cannot call the API without an image, aborting..`);
+    throw new Error(`cannot call the API without an image, aborting..`);
   }
 
   try {
     const request = {
       prompt,
-      image: imageBase64
-
-    } as ImageAnalysisRequest
+      image: imageBase64,
+    } as ImageAnalysisRequest;
 
     console.log(`calling ${apiUrl}/analyze called with: `, {
       prompt: request.prompt,
-      image: request.image.slice(0, 20)
-    })
+      image: request.image.slice(0, 20),
+    });
 
     const res = await fetch(`${apiUrl}/analyze`, {
       method: "POST",
@@ -40,19 +37,19 @@ export async function see({
         // Authorization: `Bearer ${videochainApi}`,
       },
       body: JSON.stringify(request),
-      cache: 'no-store',
-    // we can also use this (see https://vercel.com/blog/vercel-cache-api-nextjs-cache)
-    // next: { revalidate: 1 }
-    })
+      cache: "no-store",
+      // we can also use this (see https://vercel.com/blog/vercel-cache-api-nextjs-cache)
+      // next: { revalidate: 1 }
+    });
 
     if (res.status !== 200) {
-      throw new Error('Failed to fetch data')
+      throw new Error("Failed to fetch data");
     }
 
-    const response = (await res.json()) as ImageAnalysisResponse
-    return response.result
+    const response = (await res.json()) as ImageAnalysisResponse;
+    return response.result;
   } catch (err) {
-    console.error(err)
-    return ""
+    console.error(err);
+    return "";
   }
 }
