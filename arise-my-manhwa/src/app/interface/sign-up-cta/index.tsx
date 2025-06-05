c0e60e377db98a3ff2288ef31a2626e762fc41f3
DeveloperMindset123 <dasa60196@gmail.com>
2025-06-05 11:39:36 -0400

    chore:commented signup-call-to-action component not being utilized and replaced with placeholder text and TODO statement.

--- arise-my-manhwa/src/app/interface/sign-up-cta/index.tsx
+++ arise-my-manhwa/src/app/interface/sign-up-cta/index.tsx
@@ -1,8 +1,13 @@
-"use client"
+"use client";
 
 import dynamic from "next/dynamic";
 
-export const SignUpCTA = dynamic(() => import("./sign-up-cta"), {
-  // Make sure we turn SSR off
-  ssr: false,
-});
+// export const SignUpCTA = dynamic(() => import("./sign-up-cta"), {
+//   // Make sure we turn SSR off
+//   ssr: false,
+// });
+
+// TODO : implement signup logic.
+export const SignUpCTA = () => {
+  return <div>This component does nothing! Use for testing purpose</div>;
+};
